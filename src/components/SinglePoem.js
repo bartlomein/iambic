a0054a79b11e6c1f8bae8ab0247ad50b8ab84566
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-09-03 12:25:06 -0400

    some basics done to card list and card
    some basics done to card list and card

--- src/components/SinglePoem.js
+++ src/components/SinglePoem.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Button, Card, CardBody } from "shards-react";
 
-function SinglePoem({}) {
-    
-    return <h1>{greeting}</h1>;
+function SinglePoem({ body, date, likes, likeCount, user, id }) {
+  return (
+    <div style={{ marginTop: 15 }}>
+      <Card>
+        <CardBody>
+          {body.map(line => (
+            <div>{line}</div>
+          ))}
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 export default SinglePoem;
--- src/containers/PoemsList.js
+++ src/containers/PoemsList.js
@@ -1,10 +1,24 @@
 import React from "react";
-import SinglePoem from '../components/SinglePoem'
-function PoemsList({data}) {
+import SinglePoem from "../components/SinglePoem";
+function PoemsList({ data }) {
   console.log(data);
 
-    return <div>  {data && data.map((poemCard)=> <SinglePoem />)}</div>}
-  
-  
+  return (
+    <div>
+      {" "}
+      {data &&
+        data.map(elem => (
+          <SinglePoem
+            body={elem.body}
+            date={elem.createdAt}
+            likes={elem.likes}
+            likeCount={elem.likeCount}
+            user={elem.username}
+            id={elem.id}
+          />
+        ))}
+    </div>
+  );
+}
 
 export default PoemsList;
