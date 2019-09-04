4a411d42f6fc52a2ae51991c10b4dc6af4be33e3
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-09-04 10:01:35 -0400

    added shards form and some like action changes

--- src/components/SinglePoem.js
+++ src/components/SinglePoem.js
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react';
-import { Button, Card, CardBody } from 'shards-react';
-import Like from './Like';
-import { AuthContext } from '../context/auth';
-import CommentPost from './CommentPost';
-import CommentList from '../containers/CommentsList';
+import React, { useContext } from "react";
+import { Button, Card, CardBody } from "shards-react";
+import Like from "./Like";
+import { AuthContext } from "../context/auth";
+import CommentPost from "./CommentPost";
+import CommentList from "../containers/CommentsList";
 
-function SinglePoem({ body, date, likes, likeCount, id, comments }) {
+function SinglePoem({ body, date, likes, likeCount, id, comments, username }) {
   const { user } = useContext(AuthContext);
   return (
     <div style={{ marginTop: 20 }}>
@@ -14,7 +14,7 @@ function SinglePoem({ body, date, likes, likeCount, id, comments }) {
           {body.map(line => (
             <div>{line}</div>
           ))}
-          <div style={{ textAlign: 'right' }}>{user.username}</div>
+          <div style={{ textAlign: "right" }}>{username}</div>
           <Like user={user} id={id} likeCount={likeCount} likes={likes} />
           <CommentList comments={comments} />
 
