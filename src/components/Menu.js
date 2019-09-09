069ec6c6f27968c2ebcd41111c4204bfe30e192c
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-09-09 11:40:26 -0400

    menu works

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth";
 
 const Menu = () => {
-  const [menuIsOpen, setMenuIsOpen] = useState;
+  const [menuIsOpen, setMenuIsOpen] = useState();
   const { user, logout } = useContext(AuthContext);
 
   return (
     <div>
-      TEST
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
