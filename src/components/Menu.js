8600dac1e38f02c1de9fcb28ed5ae59b5099060f
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-09-09 11:52:59 -0400

    working hard on menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,14 +3,24 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth";
 
 const Menu = () => {
-  const [menuIsOpen, setMenuIsOpen] = useState();
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   const { user, logout } = useContext(AuthContext);
 
   return (
     <div>
+      <div onClick={()=>setMenuIsOpen(!menuIsOpen)}>MENU BUTTON</div>
+      <div>iambic.dev</div>
       <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+
+      {user ? (
+        <div onClick={logout}>Logout</div>
+      ) : (
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      )}
+      <div>Contact</div>
     </div>
   );
 };
