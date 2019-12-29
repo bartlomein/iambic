6aa2ca5b8dd3edab98f4b78ca22cedb8858883ce
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-12-29 15:55:31 -0500

    working on styling and ux of loging

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './containers/Home/Home';
-import Login from './containers/Login';
-import Register from './containers/Register';
-import PoemsContainer from './containers/PoemsListContainer/PoemsListContainer';
-import SinglePoemPage from './containers/SinglePoemPage/SinglePoemPage';
-import { AuthProvider } from './context/auth';
-import AuthRoute from './utils/AuthRoute';
-import Menu from './components/Menu/Menu';
-import { Container } from 'shards-react';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./containers/Home/Home";
+import Login from "./containers/Login/Login";
+import Register from "./containers/Register";
+import PoemsContainer from "./containers/PoemsListContainer/PoemsListContainer";
+import SinglePoemPage from "./containers/SinglePoemPage/SinglePoemPage";
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./utils/AuthRoute";
+import Menu from "./components/Menu/Menu";
 
 const App = () => {
   const [generatedPoem, setGeneratedPoem] = useState([]);
@@ -20,14 +19,14 @@ const App = () => {
           <Menu />
           <Route
             exact
-            path='/'
+            path="/"
             component={Home}
             setGeneratedPoem={setGeneratedPoem}
           />
-          <Route exact path='/poems' component={PoemsContainer} />
-          <Route exact path='/poems/:poemId' component={SinglePoemPage} />
-          <AuthRoute exact path='/login' component={Login} />
-          <AuthRoute exact path='/register' component={Register} />
+          <Route exact path="/poems" component={PoemsContainer} />
+          <Route exact path="/poems/:poemId" component={SinglePoemPage} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
         </Router>
       </AuthProvider>
     </div>
---
+++ src/containers/Login/LoginStyles.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+export const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+export const LoginCardContainer = styled.div`
+  max-width: 100%;
+  width: 60%;
+  margin-top: 20px;
+`;
