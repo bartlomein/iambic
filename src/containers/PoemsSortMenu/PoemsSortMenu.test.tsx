eda2bb8dff3b7927457492f1004c9b4e2be5428e
bartlomein <bartlomein123@gmail.com>
2020-01-28 11:36:54 -0500

    added a test for sort menu

--- src/containers/PoemsSortMenu/PoemsSortMenu.test.tsx
+++ src/containers/PoemsSortMenu/PoemsSortMenu.test.tsx
@@ -11,6 +11,7 @@ describe("Sort Menu", () => {
     const wrapper = shallow(<PoemsSortMenu handleSortBy={jest.fn()} />);
 
     const menuContainer = wrapper.find(".sort-menu-container");
-    expect(menuContainer).toHaveLength(sortMenuList.length);
+    console.log(menuContainer.children());
+    expect(menuContainer.children()).toHaveLength(sortMenuList.length);
   });
 });
